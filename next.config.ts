baaf3b6a16e5164e636599a4a26eeb8dcc431b00
gorrubenyan <g_rubenyan@mail.ru>
2025-07-25 00:26:37 +0400

    configs

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     devIndicators: false,
-    // eslint: {
-    //     ignoreDuringBuilds: true,
-    // }, typescript: {
-    //     ignoreBuildErrors: true,
-    // },
 
-
-    // output: 'export', // Ստատիկ կայք է ստեղծվելու
-    // // Եթե նախագիծդ արդեն ունի basePath, ավելացրու
-    // basePath: '/NextJS-CV-Profile', // փոխիր քո ռեպոյի անունով
-    // Image optimization-ը անջատելու համար (եթե օգտագործում ես next/image)
-    // images: {
-    //     unoptimized: true,
-    // },
 };
 
 export default nextConfig;
